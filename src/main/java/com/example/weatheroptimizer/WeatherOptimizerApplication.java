88256eab6af2d2089e76543d8a2e06ece7eae6ce
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-05-07 09:30:31 +0200

    initial commit

---
+++ src/main/java/com/example/weatheroptimizer/WeatherOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatheroptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherOptimizerApplication.class, args);
+	}
+
+}
