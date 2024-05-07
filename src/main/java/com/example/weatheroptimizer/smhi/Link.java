b7b68dbc2a4f838044b2db78c2273cefefd334a5
Depinder Kaur <depinder.kaur@iths.se>
2024-05-07 12:06:05 +0200

    added smhi package with necessary classes

--- src/main/java/com/example/weatheroptimizer/smhi/Link.java
+++ src/main/java/com/example/weatheroptimizer/smhi/Link.java
@@ -1,13 +1,14 @@
-package com.example.weatheroptimization.smhi;
+package com.example.weatheroptimizer.smhi;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
--- src/main/java/com/example/weatheroptimizer/smhi/Station.java
+++ src/main/java/com/example/weatheroptimizer/smhi/Station.java
@@ -1,4 +1,4 @@
-package com.example.weatheroptimization.smhi;
+package com.example.weatheroptimizer.smhi;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
