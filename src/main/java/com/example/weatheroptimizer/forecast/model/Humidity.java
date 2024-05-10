483d6e20e48a72b94181c282ee9824ce7232a1e6
Depinder Kaur <depinder.kaur@iths.se>
2024-05-10 09:09:33 +0200

    Created records: "Humidity" and "Temperature" to store both the value and its unit. These are used as fields in the WeatherData class.

---
+++ src/main/java/com/example/weatheroptimizer/forecast/model/Humidity.java
@@ -0,0 +1,4 @@
+package com.example.weatheroptimizer.forecast.model;
+
+public record Humidity(Integer value, String unit) {
+}
---
+++ src/main/java/com/example/weatheroptimizer/forecast/model/Temperature.java
@@ -0,0 +1,4 @@
+package com.example.weatheroptimizer.forecast.model;
+
+public record Temperature(Double value, String unit) {
+}
