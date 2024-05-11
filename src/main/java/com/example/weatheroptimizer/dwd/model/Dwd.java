53e7d790014a1b54d1d5a9412039c907b48fdb88
Depinder Kaur <depinder.kaur@iths.se>
2024-05-11 15:17:50 +0200

    Created a new package "dwd" which contains respective POJO classes for the Dwd data in its model package.

---
+++ src/main/java/com/example/weatheroptimizer/dwd/model/Dwd.java
@@ -0,0 +1,149 @@
+package com.example.weatheroptimizer.dwd.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "latitude",
+    "longitude",
+    "generationtime_ms",
+    "utc_offset_seconds",
+    "timezone",
+    "timezone_abbreviation",
+    "elevation",
+    "hourly_units",
+    "hourly"
+})
+
+public class Dwd {
+
+    @JsonProperty("latitude")
+    private Double latitude;
+    @JsonProperty("longitude")
+    private Double longitude;
+    @JsonProperty("generationtime_ms")
+    private Double generationTimeMs;
+    @JsonProperty("utc_offset_seconds")
+    private Integer utcOffsetSeconds;
+    @JsonProperty("timezone")
+    private String timezone;
+    @JsonProperty("timezone_abbreviation")
+    private String timezoneAbbreviation;
+    @JsonProperty("elevation")
+    private Double elevation;
+    @JsonProperty("hourly_units")
+    private HourlyUnits hourlyUnits;
+    @JsonProperty("hourly")
+    private Hourly hourly;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("latitude")
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    @JsonProperty("latitude")
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    @JsonProperty("longitude")
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    @JsonProperty("longitude")
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    @JsonProperty("generationtime_ms")
+    public Double getGenerationTimeMs() {
+        return generationTimeMs;
+    }
+
+    @JsonProperty("generationtime_ms")
+    public void setGenerationTimeMs(Double generationTimeMs) {
+        this.generationTimeMs = generationTimeMs;
+    }
+
+    @JsonProperty("utc_offset_seconds")
+    public Integer getUtcOffsetSeconds() {
+        return utcOffsetSeconds;
+    }
+
+    @JsonProperty("utc_offset_seconds")
+    public void setUtcOffsetSeconds(Integer utcOffsetSeconds) {
+        this.utcOffsetSeconds = utcOffsetSeconds;
+    }
+
+    @JsonProperty("timezone")
+    public String getTimezone() {
+        return timezone;
+    }
+
+    @JsonProperty("timezone")
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    @JsonProperty("timezone_abbreviation")
+    public String getTimezoneAbbreviation() {
+        return timezoneAbbreviation;
+    }
+
+    @JsonProperty("timezone_abbreviation")
+    public void setTimezoneAbbreviation(String timezoneAbbreviation) {
+        this.timezoneAbbreviation = timezoneAbbreviation;
+    }
+
+    @JsonProperty("elevation")
+    public Double getElevation() {
+        return elevation;
+    }
+
+    @JsonProperty("elevation")
+    public void setElevation(Double elevation) {
+        this.elevation = elevation;
+    }
+
+    @JsonProperty("hourly_units")
+    public HourlyUnits getHourlyUnits() {
+        return hourlyUnits;
+    }
+
+    @JsonProperty("hourly_units")
+    public void setHourlyUnits(HourlyUnits hourlyUnits) {
+        this.hourlyUnits = hourlyUnits;
+    }
+
+    @JsonProperty("hourly")
+    public Hourly getHourly() {
+        return hourly;
+    }
+
+    @JsonProperty("hourly")
+    public void setHourly(Hourly hourly) {
+        this.hourly = hourly;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ src/main/java/com/example/weatheroptimizer/dwd/model/HourlyUnits.java
@@ -0,0 +1,84 @@
+package com.example.weatheroptimizer.dwd.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "time",
+    "temperature_2m",
+    "relative_humidity_2m",
+    "precipitation"
+})
+
+public class HourlyUnits {
+
+    @JsonProperty("time")
+    private String time;
+    @JsonProperty("temperature_2m")
+    private String temperature2m;
+    @JsonProperty("relative_humidity_2m")
+    private String relativeHumidity2m;
+    @JsonProperty("precipitation")
+    private String precipitation;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("time")
+    public String getTime() {
+        return time;
+    }
+
+    @JsonProperty("time")
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @JsonProperty("temperature_2m")
+    public String getTemperature2m() {
+        return temperature2m;
+    }
+
+    @JsonProperty("temperature_2m")
+    public void setTemperature2m(String temperature2m) {
+        this.temperature2m = temperature2m;
+    }
+
+    @JsonProperty("relative_humidity_2m")
+    public String getRelativeHumidity2m() {
+        return relativeHumidity2m;
+    }
+
+    @JsonProperty("relative_humidity_2m")
+    public void setRelativeHumidity2m(String relativeHumidity2m) {
+        this.relativeHumidity2m = relativeHumidity2m;
+    }
+
+    @JsonProperty("precipitation")
+    public String getPrecipitation() {
+        return precipitation;
+    }
+
+    @JsonProperty("precipitation")
+    public void setPrecipitation(String precipitation) {
+        this.precipitation = precipitation;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
