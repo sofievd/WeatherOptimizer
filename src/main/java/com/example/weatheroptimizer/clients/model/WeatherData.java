e517ddf8f1d0c7b5e81f0eb74d6343c8f5c2d028
Depinder Kaur <depinder.kaur@iths.se>
2024-05-13 13:16:37 +0200

    Slight modification.

--- src/main/java/com/example/weatheroptimizer/clients/model/WeatherData.java
+++ src/main/java/com/example/weatheroptimizer/clients/model/WeatherData.java
@@ -8,8 +8,9 @@ public record WeatherData(
         WeatherSource source,
         Temperature temperature,
         Humidity humidity,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
-        LocalDateTime validTime) implements Comparable<WeatherData> {
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm")
+        LocalDateTime validTime)
+        implements Comparable<WeatherData> {
     @Override
     public int compareTo(WeatherData data) {
         return temperature.compareTo(data.temperature());
